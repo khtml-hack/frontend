d1bdb8a16b87f928a72912fc564f0d384bb16ce0
sieun-front <030107hse@gmail.com>
2025-08-29 20:14:03 +0900

    웰컴페이지 css 조정

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,19 +7,19 @@ const Welcome = () => {
     return (
         <div className="mobile-frame">
             {/* Content */}
-            <div className="flex flex-col items-center justify-center h-full px-auto mt-[250px]">
+            <div className="flex flex-col items-center justify-center h-full px-auto mt-[270px]">
                 <div className="flex-1 flex flex-col items-center justify-center">
-                    {{ PeakDownLogo } && <img src={PeakDownLogo} alt="PeakDown Logo" className="w-35 h-37 mb-8" />}
+                    {{ PeakDownLogo } && <img src={PeakDownLogo} alt="PeakDown Logo" className="w-35 h-37 mb-[45px]" />}
                     <div className="flex flex-col items-center space-y-4 ">
                         <button
                             onClick={() => navigate('/login')}
-                            className="bg-[#363636] text-white rounded-2xl py-2.5 px-10 w-[164px] text-xl font-medium"
+                            className="bg-[#363636] text-white rounded-2xl py-2.5 px-10 w-[180px] text-xl font-medium"
                         >
                             로그인
                         </button>
                         <button
                             onClick={() => navigate('/signup')}
-                            className="bg-[#363636] text-white rounded-2xl py-2.5 px-10 w-[164px] text-xl font-medium text-center"
+                            className="bg-[#363636] text-white rounded-2xl py-2.5 px-10 w-[180px] text-xl font-medium text-center"
                         >
                             회원가입
                         </button>
